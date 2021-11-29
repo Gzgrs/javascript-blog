2299920d4424ae2f6dd784ead444eee25e37f6f4
Grzegorz Siudak <siudak.grzegorz@gmail.com>
2021-11-29 21:24:19 +0000

    Get 'href' attribute from the clicked link

--- js/script.js
+++ js/script.js
@@ -6,18 +6,24 @@
 });
 */
 const titleClickHandler = function(event){
+
+  event.preventDefault();
+
   const clickedElement = this;
+
   console.log('Link was clicked!');
+
   console.log(event);
     /* [Done] remove class 'active' from all article links  */
 const activeLinks = document.querySelectorAll('.titles a.active');
 
 for(let activeLink of activeLinks){
+
   activeLink.classList.remove('active');
 }
-    /* [IN PROGRESS] add class 'active' to the clicked link */
+    /* [Done] add class 'active' to the clicked link */
 clickedElement.classList.add('active');
-console.log('clickedElement:', clickedElement);
+  console.log('clickedElement:', clickedElement);
     /* [DONE] remove class 'active' from all articles */
 const activeArticles = document.querySelectorAll('.posts article.active');
 
@@ -25,6 +31,9 @@ for(let activeArticle of activeArticles){
   activeArticle.classList.remove('active');
 }
     /* get 'href' attribute from the clicked link */
+
+const href = clickedElement.getAttribute('href')
+const articleSelector = document.querySelector('href')
 
     /* find the correct article using the selector (value of 'href' attribute) */
 
